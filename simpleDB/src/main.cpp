98931c89e352a8e0033df532321c785c8abeefed
8evgeny <8evgeny@gmail.com>
2021-01-29 18:23:07 +0300

    не работает

--- simpleDB/src/main.cpp
+++ simpleDB/src/main.cpp
@@ -1,6 +1,11 @@
 #include <conio.h>
-#include <fcntl.h>
-#include <io.h>
+
+#include <io.h>      // для функции _setmode
+#include <fcntl.h>   // для константы _O_U16TEXT
+#include <fstream>   // для файлового ввода/вывода
+#include <iostream>
+#include <string>    // для работы с классом wstring
+#include <codecvt>   // для работы с фасетом codecvt_utf8 локали
 
 #include <clocale>
 #include <sstream>
@@ -17,10 +22,40 @@ using namespace std;
 //  }
 //};
 
-int wmain() {
-  _setmode(_fileno(stdout), _O_U16TEXT);
-  _setmode(_fileno(stdin), _O_U16TEXT);
-  _setmode(_fileno(stderr), _O_U16TEXT);
+int main() {
+
+  _setmode(_fileno(stdout), _O_U8TEXT);
+  _setmode(_fileno(stdin), _O_U8TEXT);
+  _setmode(_fileno(stderr), _O_U8TEXT);
+
+  const locale utf8_locale = locale(locale(),	new codecvt_utf8<wchar_t>());
+
+  wchar_t ch = L'ы';                        // русская буква 'ы'
+  int j = 77;
+  double d = 6.02;
+  wstring str1 = L"Kafka";                  // две строки (одна — с латинскими буквами,
+  wstring str2 = L"Пруст";                  // другая — с русскими) без пробелов
+
+ std:: wofstream outfile("файл с данными.txt"); // создать объект класса wofstream
+  outfile.imbue(utf8_locale);               // связываем наш поток с нужной локалью
+
+  outfile << ch                             // вставить (записать) данные
+          << j
+          << L' '                           // числа разделим пробелом
+          << d
+          << str1
+          << L' '                           // строки тоже разделим пробелом
+          << str2;
+
+  wcout << L"Файл записан\n";
+
+
+
+
+
+
+
+
 
   //  SetConsoleCP(CP_UTF8);
   //  SetConsoleOutputCP(CP_UTF8);
